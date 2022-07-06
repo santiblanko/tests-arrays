0dec017ecd56700f786f4c18fe7ab2459a6d1083
santiago <santiago.blanco@meltstudio.co>
2022-07-06 12:43:11 -0500

    Add Example 4

---
+++ example-4.js
@@ -0,0 +1,36 @@
+import { cleanConsole, createAll } from "./data"
+const companies = createAll()
+
+cleanConsole(4, companies)
+
+
+// -----------------------------------------------------------------------------
+// INSTRUCCIONES EN ESPAÑOL
+
+// Crear una función tomando como parámetro la variable "companies" y agrupando
+// todos los "users" de todas las "companies" en una sola tabla. Cada "user"
+// debe tener un nuevo atributo "company" que tenga como valor el nombre de la
+// dicha "company". Los "users" deben ordenarse de acuerdo con sus edad
+// (de mayor a menor).
+
+export function getUserData(companies) {
+  let users = []
+  companies.forEach(company => {
+    company.users.forEach(user => {
+      user.company = company.name
+      users.push(user)
+    })
+  })
+  users.sort((a,b)=> b.age - a.age)
+  return users
+}
+
+console.log("---- EXAMPLE 4 --- ", getUserData(companies))
+// -----------------------------------------------------------------------------
+// INSTRUCTIONS IN ENGLISH
+
+// Create a function taking as parameter the "companies" variable and grouping
+// all "users" of all "companies" in a single table. Each "user"
+// must have a new attribute "company" having for value the name of the "company"
+// to which it belongs. The "users" must be sorted according to their
+// age (from oldest to youngest)
